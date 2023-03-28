80e0461995929048a7640e428fb57546a54292a5
wutthichai <qqwut@hotmail.com>
2023-03-28 15:09:10 +0700

    change api env

--- src/environments/environment.uat.ts
+++ src/environments/environment.uat.ts
@@ -22,7 +22,7 @@ export const environment = {
   apilookup: 'https://centerapi.philliplife.com/apiproxy2/lookupapi/api/lookup/',
   apiclaim: 'https://centerapi.philliplife.com/apiproxy2/docuclaim.api/api/claim/',
   apiclaimreport: 'https://centerapi.philliplife.com/apiproxy2/ClaimApiDev/api/report/',
-  apiReport: 'https://centerapi.philliplife.com/apiproxy2/docuclaim.apii/api/',
+  apiReport: 'https://centerapi.philliplife.com/apiproxy2/docuclaim.api/api/',
   //menuesValidated = เพื่อให้ระบบทำการ flag เป็น true เมื่อ login ผ่านแล้ว และมีการตรวจสอบสิทธิของเมนูแล้ว **กรณีกดปุ่ม refresh จะกลับมาเป็น false ใน AuthGuard จะทำการตรวจสอบ menu ใหม่
   menuesValidated: false,
   menues: [
